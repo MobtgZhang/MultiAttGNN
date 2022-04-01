7396a39625f17bad8195dfc1efa9b80add843cb3
MobtgZhang <3215497340@qq.com>
2022-04-01 15:46:01 +0800

    Add files via upload

--- src/data.py
+++ src/data.py
@@ -6,9 +6,32 @@ class Dictionary:
         self.name = 'default'
         self.ind2token = ['<PAD>','<START>','<END>','<UNK>',]
         self.token2ind = {'<PAD>':0,'<START>':1,'<END>':2,'<UNK>':3}
-        self.label_token = ['<PAD>','<START>','<END>','<UNK>',]
+        self.token2val = {'<PAD>':1.0,'<START>':0.5,'<END>':0.5,'<UNK>':2.0}
+        self.stopwords = set()
         self.start_index = 0
         self.end_index = len(self.ind2token)
+    @property
+    def pad(self):
+        return self.ind2token[0]
+    @property
+    def start(self):
+        return self.ind2token[1]
+    @property
+    def end(self):
+        return self.ind2token[2]
+    @property
+    def unk(self):
+        return self.ind2token[3]
+    def add_stopwords(self,word):
+        if word == None:
+            return
+        if type(word)==str:
+            self.token2val[word] = 1.25
+        elif type(word)==set:
+            for w in word:
+                self.token2val[w] = 1.25
+        else:
+            raise TypeError("Unknown model type %s"%str(type(word)))
     def __iter__(self):
         return self
     def __next__(self):
@@ -20,7 +43,7 @@ class Dictionary:
             raise StopIteration
     def __getitem__(self,item):
         if type(item) == str:
-            return self.token2ind.get(item,self.token2ind['<UNK>'])
+            return self.token2ind.get(item,self.token2ind[self.unk])
         elif type(item) == int:
             word = self.ind2token[item]
             return word
@@ -31,11 +54,18 @@ class Dictionary:
             self.token2ind[word] = len(self.ind2token)
             self.ind2token.append(word)
             self.end_index = len(self.ind2token)
+            if word in self.stopwords:
+                self.token2val[word] = 1.25
+            else:
+                self.token2val[word] = 1.5
+    def get_value(self,word):
+        return self.token2val.get(word,self.token2val[self.unk])
     def save(self,save_file):
         with open(save_file,"wb") as wfp:
             data = {
                 "ind2token":self.ind2token,
                 "token2ind":self.token2ind,
+                "token2val":self.token2val
             }
             pickle.dump(data,wfp)
     @staticmethod
@@ -45,6 +75,7 @@ class Dictionary:
             data = pickle.load(rfp)
             tp_dict.token2ind = data["token2ind"]
             tp_dict.ind2token = data["ind2token"]
+            tp_dict.token2val = data["token2val"]
             tp_dict.end_index = len(tp_dict.ind2token)
         return tp_dict
     def __contains__(self,word):
@@ -58,6 +89,10 @@ class Dictionary:
     def __str__(self) -> str:
         return '{}(num_keys={})'.format(
             self.__class__.__name__,len(self.token2ind))
+
+
+
+            
 class SentimentDataset:
     def __init__(self,x_features,x_adj,y_labels):
         pass
--- src/utils.py
+++ src/utils.py
@@ -1,50 +1,216 @@
 import os
 import pickle
-import jieba
-import random
 import math
+import json
+
+import jieba
+from janome.tokenizer import Tokenizer
 
 from tqdm import tqdm
 import numpy as np
 import scipy.sparse as ssp
 import pandas as pd
+import sklearn
 
 from .data import Dictionary
-from .adjacency import normalize_adj
 
-def build_dataset(dataset_file,save_dataset_file,sent_name):
-    dataset = pd.read_csv(dataset_file)
-    all_data = list(map(list,list(dataset[sent_name].map(jieba.cut))))
-    labels = dataset.drop(labels='id',axis=1).drop(labels=sent_name,axis=1).to_numpy()
-    idx = dataset['id'].to_numpy()
-    labels = labels+2
-    all_process_data = {
-        "idx":idx,
-        "content":all_data,
-        "labels":labels
-    }
-    with open(save_dataset_file,mode="wb") as wfp:
-        pickle.dump(all_process_data,wfp)
+def build_aichallenger_dataset(dataset_dir,reusult_dir):
+    load_train_file = os.path.join(dataset_dir,"sentiment_analysis_trainingset.csv")
+    load_validate_file = os.path.join(dataset_dir,"sentiment_analysis_validationset.csv")
+    train_dataset = pd.read_csv(load_train_file)
+    dataset = pd.read_csv(load_validate_file)
+    dataset = sklearn.utils.shuffle(dataset)
+    val_len = len(dataset)//2
+    val_dataset = dataset.iloc[:val_len,:]
+    test_dataset = dataset.iloc[val_len:,:]
+    names_list = ['train','validate','test']
+    dataset_list = [train_dataset,val_dataset,test_dataset]
+    for name,tp_dataset in tqdm(zip(names_list,dataset_list),desc='processing aichallenger dataset'):
+        save_dataset_file = os.path.join(reusult_dir,"%s_dataset.pkl"%name)
+        if os.path.exists(save_dataset_file):
+            continue
+        all_data = list(map(list,list(tp_dataset['content'].map(jieba.cut))))
+        labels = tp_dataset.drop(labels='id',axis=1).drop(labels='content',axis=1).to_numpy()
+        idx = tp_dataset['id'].to_numpy()
+        labels = labels+2
+        all_process_data = {
+            "idx":idx,
+            "content":all_data,
+            "labels":labels
+        }
+        with open(save_dataset_file,mode="wb") as wfp:
+            pickle.dump(all_process_data,wfp)
+def build_wrime_dataset(load_dataset_file,result_data_dir,label_name='writer',wrime_name="v1"):
+    dataset = pd.read_csv(load_dataset_file,sep='\t')
+    columns_list = []
+    for label in dataset.columns.values:
+        if label_name in label.lower():
+            columns_list.append(label)
+    train_dataset = dataset[dataset['Train/Dev/Test']=='train']
+    dev_dataset = dataset[dataset['Train/Dev/Test']=='dev']
+    test_dataset = dataset[dataset['Train/Dev/Test']=='test']
+    names_list = ['train','validate','test']
+    dataset_list = [train_dataset,dev_dataset,test_dataset]
+    for name,tp_dataset in tqdm(zip(names_list,dataset_list),desc="processing WRIME-v2 dataset"):
+        token_tp = Tokenizer()
+        content = list(tp_dataset["Sentence"].map(lambda x:[item.surface for item in token_tp.tokenize(x)]))
+        index = tp_dataset.index.to_numpy()
+        labels = tp_dataset[columns_list].to_numpy()
+        all_data = {
+            "idx":index,
+            "content":content,
+            "labels":labels
+        }
+        save_file_name = os.path.join(result_data_dir,'%s_dataset.pkl'%name)
+        with open(save_file_name,mode='wb') as wfp:
+            pickle.dump(all_data,wfp)
+def build_weibo4moods(load_data_file,result_data_dir):
+    train_per = 0.64
+    val_per = 0.82
+    dataset = pd.read_csv(load_data_file)
+    dataset = sklearn.utils.shuffle(dataset)
+    train_len = int(train_per*len(dataset))
+    val_len = int(val_per*len(dataset))
+    train_dataset = dataset.iloc[:train_len,:]
+    validate_dataset = dataset.iloc[train_len:val_len,:]
+    test_dataset = dataset.iloc[val_len:,:]
+    names_list = ['train','validate','test']
+    dataset_list = [train_dataset,validate_dataset,test_dataset]
+    for name,tp_dataset in tqdm(zip(names_list,dataset_list),desc='processing weibo4moods dataset'):
+        save_file_name = os.path.join(result_data_dir,'%s_dataset.pkl'%name)
+        content = list(map(list,list(tp_dataset['review'].map(jieba.cut))))
+        index = tp_dataset.index.to_numpy()
+        labels = tp_dataset['label'].to_numpy()
+        all_data = {
+            "idx":index,
+            "content":content,
+            "labels":labels
+        }
+        with open(save_file_name,mode='wb') as wfp:
+            pickle.dump(all_data,wfp)
+def build_student(load_data_file,result_data_dir):
+    train_per = 0.64
+    val_per = 0.82
+    dataset = pd.read_excel(load_data_file)
+    dataset = sklearn.utils.shuffle(dataset)
+    train_len = int(train_per*len(dataset))
+    val_len = int(val_per*len(dataset))
+    train_dataset = dataset.iloc[:train_len,:]
+    validate_dataset = dataset.iloc[train_len:val_len,:]
+    test_dataset = dataset.iloc[val_len:,:]
+    names_list = ['train','validate','test']
+    dataset_list = [train_dataset,validate_dataset,test_dataset]
+    for name,tp_dataset in tqdm(zip(names_list,dataset_list),desc='processing student dataset'):
+        save_file_name = os.path.join(result_data_dir,'%s_dataset.pkl'%name)
+        content = list(map(list,list(tp_dataset['content'].map(jieba.cut))))
+        index = tp_dataset['总序号'].to_numpy()
+        labels = tp_dataset['态度标签'].to_numpy()-1
+        all_data = {
+            "idx":index,
+            "content":content,
+            "labels":labels
+        }
+        with open(save_file_name,mode='wb') as wfp:
+            pickle.dump(all_data,wfp)
+        
+def build_clue2020emotions_dataset(dataset_dir,result_data_dir):
+    names_list = ['train','validate','test']
+    for name in tqdm(names_list,desc='processing student dataset'):
+        load_filename = os.path.join(dataset_dir,"%s.txt"%name)
+        save_file_name = os.path.join(result_data_dir,"%s_dataset.pkl"%name)
+        dataset = []
+        with open(load_filename,mode="r",encoding="utf-8") as rfp:
+            for line in rfp:
+                data_dict = json.loads(line)
+                dataset.append(data_dict)
+            dataset = pd.DataFrame(dataset)
+            content = list(map(list,list(dataset['content'].map(jieba.cut))))
+            index = dataset['id'].to_numpy()
+            labels = dataset['label']
+            all_data = {
+                "idx":index,
+                "content":content,
+                "labels":labels
+            }
+            with open(save_file_name,mode='wb') as wfp:
+                pickle.dump(all_data,wfp)
+def build_goemotions_dataset(dataset_dir,result_data_dir):
+    train_per = 0.64
+    val_per = 0.82
+    columns_list = ['admiration',
+       'amusement', 'anger', 'annoyance', 'approval', 'caring', 'confusion',
+       'curiosity', 'desire', 'disappointment', 'disapproval', 'disgust',
+       'embarrassment', 'excitement', 'fear', 'gratitude', 'grief', 'joy',
+       'love', 'nervousness', 'optimism', 'pride', 'realization', 'relief',
+       'remorse', 'sadness', 'surprise', 'neutral']
+    all_dataset = [pd.read_csv(os.path.join(dataset_dir,"goemotions_%s.csv"%str(idx))) for idx in range(1,4)]
+    all_dataset = pd.concat(all_dataset)
+    all_dataset = sklearn.utils.shuffle(all_dataset)
+    
+    train_len = int(train_per*len(all_dataset))
+    val_len = int(val_per*len(all_dataset))
+    train_dataset = all_dataset.iloc[:train_len,:]
+    validate_dataset = all_dataset.iloc[train_len:val_len,:]
+    test_dataset = all_dataset.iloc[val_len:,:]
+    names_list = ['train','validate','test']
+    dataset_list = [train_dataset,validate_dataset,test_dataset]
+    for name,tp_dataset in tqdm(zip(names_list,dataset_list),desc='processing student dataset'):
+        save_file_name = os.path.join(result_data_dir,'%s_dataset.pkl'%name)
+        content = list(map(list,list(tp_dataset['text'].map(lambda x:x.split(' ')))))
+        index = tp_dataset['id']
+        labels = tp_dataset[columns_list]
+        all_data = {
+            "idx":index,
+            "content":content,
+            "labels":labels
+        }
+        with open(save_file_name,mode='wb') as wfp:
+            pickle.dump(all_data,wfp)
+def build_SST5_dataset(dataset_dir,result_data_dir):
+    load_train_file = os.path.join(dataset_dir,"train.txt")
+    load_valid_file = os.path.join(dataset_dir,"dev.txt")
+    load_test_file = os.path.join(dataset_dir,"test.txt")
+    columns = ["label","sentence"]
+    train_dataset = pd.read_csv(load_train_file,sep='\t',header=None)
+    train_dataset.columns = columns
+    validate_dataset = pd.read_csv(load_valid_file,sep='\t',header=None)
+    validate_dataset.columns = columns
+    test_dataset = pd.read_csv(load_test_file,sep='\t',header=None)
+    test_dataset.columns = columns
+    names_list = ['train','validate','test']
+    dataset_list = [train_dataset,validate_dataset,test_dataset]
+    for name,tp_dataset in tqdm(zip(names_list,dataset_list),desc='processing student dataset'):
+        save_file_name = os.path.join(result_data_dir,'%s_dataset.pkl'%name)
+        content = list(map(list,list(tp_dataset['sentence'].map(lambda x:x.split()))))
+        index = tp_dataset.index.to_numpy()
+        labels = tp_dataset["label"].to_numpy()
+        all_data = {
+            "idx":index,
+            "content":content,
+            "labels":labels
+        }
+        with open(save_file_name,mode='wb') as wfp:
+            pickle.dump(all_data,wfp)
 
-def build_dictionary(result_dir):
+def build_dictionary(result_dir,stop_words=None):
     words_dict = Dictionary()
     chars_dict = Dictionary()
-    save_train_dataset_file = os.path.join(result_dir,"train.pkl")
-    save_validate_dataset_file = os.path.join(result_dir,"validate.pkl")
-    listed_dirs = [save_train_dataset_file,save_validate_dataset_file]
-    for file_name in listed_dirs:
-        with open(save_train_dataset_file,mode="rb") as rfp:
+    listed_names= ['train','validate']
+    for name in listed_names:
+        save_file_name = os.path.join(result_dir,"%s_dataset.pkl"%name)
+        with open(save_file_name,mode="rb") as rfp:
             all_data = pickle.load(rfp)
-        for idx in tqdm(range(len(all_data["content"])),desc='processing file:%s'%file_name):
+        for idx in tqdm(range(len(all_data["content"])),desc='words processing file:%s'%save_file_name):
             for word in all_data["content"][idx]:
                 words_dict.add(word)
             for char in "".join(all_data["content"][idx]):
                 chars_dict.add(char)
     save_chars_dict_file = os.path.join(result_dir,"chars_dict.pkl")
     save_words_dict_file = os.path.join(result_dir,"words_dict.pkl")
+    words_dict.add_stopwords(stop_words)
     chars_dict.save(save_chars_dict_file)
     words_dict.save(save_words_dict_file)
-def build_embeddings(result_dir,load_embedding_file,save_embedding_file):
+def build_embeddings(result_dir,load_embedding_file,save_embedding_file,head=True):
     save_words_dataset_file = os.path.join(result_dir,"chars_dict.pkl")
     save_chars_dataset_file = os.path.join(result_dir,"words_dict.pkl")
 
@@ -53,16 +219,23 @@ def build_embeddings(result_dir,load_embedding_file,save_embedding_file):
     char_embeddings = {}
     word_embeddings = {}
     with open(load_embedding_file,mode="r",encoding="utf-8") as rfp:
-        word_nums,embedding_dim = rfp.readline().strip().split()
-        word_nums = int(word_nums)
-        embedding_dim = int(embedding_dim)
+        if head:
+            _,embedding_dim = rfp.readline().strip().split()
+            embedding_dim = int(embedding_dim)
+        else:
+            pass
+        word_nums=0
         for line in tqdm(rfp.readlines(),desc="loading embeddings"):
             data = line.split()
             word = str(data[0])
+            vecs = list(map(float,data[1:]))
+            word_nums += 1
             if word in words_dict:
-                word_embeddings[word] = np.array(list(map(float,data[1:])),dtype=np.float)
+                word_embeddings[word] = np.array(vecs,dtype=np.float)
+                embedding_dim = len(vecs)
             if word in chars_dict:
-                char_embeddings[word] = np.array(list(map(float,data[1:])),dtype=np.float)
+                char_embeddings[word] = np.array(vecs,dtype=np.float)
+                embedding_dim = len(vecs)
     for word in tqdm(words_dict,"fixing word embeddings"):
         if word not in word_embeddings:
             word_embeddings[word] = np.random.uniform(-0.01,0.01,embedding_dim)
@@ -76,24 +249,17 @@ def build_embeddings(result_dir,load_embedding_file,save_embedding_file):
     }
     with open(save_embedding_file,mode="wb") as wfp:
         pickle.dump(data_dict,wfp)
-def build_stop_words(load_stop_words_file,save_stop_words_file):
-    words_dict = Dictionary()
-    with open(load_stop_words_file,mode="r",encoding="gbk") as rfp:
-        for line in tqdm(rfp.readlines(),desc='processing stop words'):
-            word = line.strip()
-            words_dict.add(word)
-    words_dict.save(save_stop_words_file)
+def build_stop_words(load_stop_words_file,save_stop_words_file,lang='zh'):
+    words_dict = set()
+    encoding = 'utf-8'
+    with open(load_stop_words_file,mode="r",encoding=encoding) as rfp:
+        words_dict = set(json.load(rfp))
+    with open(save_stop_words_file,mode="wb") as wfp:
+        pickle.dump(words_dict,wfp)
 # build graph function
-def build_graph(words_dict,chars_dict,save_dataset_file,save_graph_file,
-            save_stopwords_file=None,window_size=3,weighted_graph=True):
+def build_graph(words_dict,chars_dict,save_dataset_file,save_graph_file,window_size=3,weighted_graph=True):
     with open(save_dataset_file,mode="rb") as rfp:
         raw_dataset = pickle.load(rfp)
-    if save_stopwords_file is not None:
-        with open(save_dataset_file,mode="rb") as rfp:
-            stop_words_dict = pickle.load(rfp)
-        stop_words_flag = True
-    else:
-        stop_words_flag = False
     all_dataset = raw_dataset['content']
     
     doc_words_features = []
@@ -115,17 +281,17 @@ def build_graph(words_dict,chars_dict,save_dataset_file,save_graph_file,
         doc_words = all_dataset[i]
         doc_chars = list("".join(doc_words))
         if len(doc_words)>seq_words_len:
-            real_words_len = seq_words_len
             doc_words = doc_words[:seq_words_len]
         else:
             pad_len = seq_words_len - len(doc_words)# padding for each epoch
-            real_words_len = len(doc_words)
-            doc_words += ['<PAD>']*pad_len
+            doc_words += [words_dict.pad]*pad_len
         if len(doc_chars)>seq_chars_len:
             doc_chars = doc_chars[:seq_chars_len]
         else:
             pad_len = seq_chars_len - len(doc_chars)
-            doc_chars += ['<PAD>']*pad_len
+            doc_chars += [words_dict.pad]*pad_len
+        doc_words = [words_dict.start] + doc_words + [words_dict.end]
+        doc_chars = [words_dict.start] + doc_chars + [words_dict.end]
         ################################################################
         doc_len = len(doc_words)
         doc_len_list.append(doc_len)
@@ -153,14 +319,9 @@ def build_graph(words_dict,chars_dict,save_dataset_file,save_graph_file,
                     if word_p_id == word_q_id:
                         continue
                     word_pair_key = (word_p_id, word_q_id)
-                    value = 0.0
-                    if word_p not in words_dict.label_token:
-                        if stop_words_flag and word_p not in stop_words_dict:
-                            value += 0.5
-                    if word_q not in words_dict.label_token:
-                        if stop_words_flag and word_p not in stop_words_dict:
-                            value += 0.5
                     
+                    value = words_dict.get_value(word_p) + words_dict.get_value(word_q)
+
                     # word co-occurrences as weights
                     if word_pair_key in word_pair_count:
                         word_pair_count[word_pair_key] += value
@@ -190,7 +351,7 @@ def build_graph(words_dict,chars_dict,save_dataset_file,save_graph_file,
         mask = []
         for word in doc_words:
             words_features.append(words_dict[word])
-        mask = [0 if word in stop_words_dict or word in words_dict.label_token else 1 for word in doc_words]
+        mask = [0 if word in words_dict.ind2token else 1 for word in doc_words]
         mask = np.array(mask,dtype=np.int64)
         for char in doc_chars:
             chars_features.append(chars_dict[char])
